30f5f8c9900020a68c4e79e825112201a193ed39
SergGurkov95 <Serg.Gurkov95@gmail.com>
2021-01-30 12:20:44 +0200

    try/catch/finally

---
+++ src/Demo.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+
+public class Demo {
+    public static String read(String path) {
+
+        boolean missingFile = false;
+        StringBuilder stringBuilder = new StringBuilder();
+
+//открываем файл по пути
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String currentString;
+//построчно считываем файл
+            while ((currentString = reader.readLine()) != null) {
+                stringBuilder.append(currentString);
+                stringBuilder.append("\n");
+            }
+//закрываем файл
+            reader.close();
+
+        } catch (IOException exc) {
+            missingFile = true;
+        } finally {
+            if (missingFile) {          //Если файл не найден
+                return "Проверьте наличие файла по указанному пути: \"" + path + "\"";
+            } else                      //возвращаем вычитанный текст в строке
+                return stringBuilder.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(read("C:\\test.txt"));
+    }
+}
